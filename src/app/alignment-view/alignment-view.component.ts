48345c77a1274fa43975b75efe9d263c90ffd753
guidou3 <guido.santi@studenti.unipd.it>
2020-03-25 23:15:46 +0100

    1 file modified

--- src/app/alignment-view/alignment-view.component.ts
+++ src/app/alignment-view/alignment-view.component.ts
@@ -6,14 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alignment-view.component.css']
 })
 export class AlignmentViewComponent implements OnInit {
+  private paths;
+  constructor() { 
+    let index = 1;
+    this.paths = [ 
+      {
+        length: 50,
+        color: "white",
+        path: 'M 0 0 l 50 0 l 30 25 l -30 25 l -50 0 z'
+      },
+      {
+        length: 50,
+        color: "white",
+        path: 'M 55 0 l 50 0 l 30 25 l -30 25 l -50 0 l 30 -25 z'
+      },
+      {
+        length: 50,
+        color: "white",
+        path: 'M 1 0 l 50 0 l 30 25 l -30 25 l -50 0 l 30 -25 z'
+      },
 
-  constructor() { }
+      
+    ]
+  }
 
   ngOnInit() {
   }
 
-  public number2() {e
+  public number2(params) {
     console.log("clicked")
+    console.log(params)
+  }
+
+  printFirst(length) {
+    return "M 0 0 l " + length + " 0 l 30 25 l -30 25 l -" + length + " 0 z"
+  }
+
+  printOthers(length, index) {
+    return "M " + index + " 0 l " + length + " 0 l 30 25 l -30 25 l -" + length + " 0 l 30 -25z"
   }
 
 }
